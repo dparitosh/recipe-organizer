988127ab22b9da2dab8fe3cd9f153c30d521e348
Paritosh Deshmukh <51744654+dparitosh@users.noreply.github.com>
2025-11-03 04:16:23 +0000

    Edited Spark

---
+++ remove-jsx-files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -f /workspaces/spark-templ
+# Remove .jsx files from src/components/
+rm -f /workspaces/spark-template/src/components/AIServ
+
+rm -f /workspaces/spark-template/src/com
+rm -f /workspaces/spark-template/src/components/SearchBar.jsx
+
+rm -f /workspaces/spark-template/src/hooks/use-mobile.js
+echo "All .jsx and duplicate .js files have been removed!"
+
+
+
+
+
+
+
+
+
+
